2d4d641602735cb35321c85f3509f3b585bb5856
Kennedy Hipolito <41745628+whoamikenken@users.noreply.github.com>
2022-11-10 08:29:12 +0800

    heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Paginator::useBootstrapFive();
+        Schema::defaultStringLength(191);
     }
 }
